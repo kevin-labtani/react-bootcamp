5089581ffc37f5371a044515805b26dbfdad3631
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-21 12:19:52 +0100

    embedd js into jsx

--- 2.JSX/index.js
+++ 2.JSX/index.js
@@ -1,14 +1,22 @@
+function getMood() {
+  const moods = ["Angry", "Sad", "Hungry", "Depressed", "Silly"];
+  return moods[Math.floor(Math.random() * moods.length)];
+}
+
 // don't forget closing tags! ans slashes in closing tags!
 class JSXDemo extends React.Component {
   render() {
     // retrun a single element
     return (
       <div>
-        <h1>My image! </h1>
+        <h2>My image!</h2>
         <img
           src="https://images.unsplash.com/photo-1552283576-3ea3519bf12e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=889&q=80"
           alt="image"
         />
+        {/* embed js into jsx with curly braces */}
+        <h2>My number is: {2 * 8.4}</h2>
+        <h2>My Current Mood is: {getMood()}</h2>
       </div>
     );
   }
