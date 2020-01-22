78bcc1bf82b66c68fe8830240b27fb616ab67c64
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-22 16:28:35 +0100

    imports/exports

---
+++ 4.create-react-app/2.import-export-demo/src/helpers.js
@@ -0,0 +1,15 @@
+function helpful() {
+  console.log("I did a helpful thing!!!");
+}
+
+function sort() {
+  console.log("All sorted");
+}
+
+function sing() {
+  console.log("lalala!");
+}
+
+// export as default
+export default helpful;
+export { sort, sing };
---
+++ 4.create-react-app/2.import-export-demo/src/index.js
@@ -0,0 +1,8 @@
+import h, { sort, sing } from "./helpers";
+
+// can directly rename the default import
+h();
+sing();
+sort();
+
+// use default exports when there's a "most likely thing" to be exported
