5c4b142640a4c2693fc85602e750daab8e9d1d0b
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-28 17:02:41 +0100

    add comments to clicker exercise

--- 6.state/1.state-demo/src/Clicker.js
+++ 6.state/1.state-demo/src/Clicker.js
@@ -1,22 +1,27 @@
+// clicker state exercise
 import React, { Component } from "react";
 
 export default class Clicker extends Component {
   constructor(props) {
     super(props);
-
+    // init state
     this.state = {
       num: 1
     };
 
+    // bind this for handler
     this.clickHandler = this.clickHandler.bind(this);
   }
 
+  // handle click
   clickHandler(e) {
+    // pick nr between 1 and 10
     let rand = Math.floor(Math.random() * 10 + 1);
     this.setState({ num: rand });
   }
 
   render() {
+    // conditional dependant on state
     let condition;
     if (this.state.num == 7) {
       condition = <h3>YOU WIN!</h3>;
