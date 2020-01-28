ec7eb503cc58c817ad26f47309b5023b42f739cc
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-28 13:16:36 +0100

    use better images

--- 5.pokedex/src/Pokecard.js
+++ 5.pokedex/src/Pokecard.js
@@ -1,12 +1,14 @@
 import React, { Component } from "react";
 import "./Pokecard.css";
 
-const POKE_API =
-  "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/";
+const POKE_API = "https://assets.pokemon.com/assets/cms2/img/pokedex/detail/";
+
+// pad the id's with 0's in front to 3 placess as that's what the api expects
+let padToThree = number => (number <= 999 ? `00${number}`.slice(-3) : number);
 
 export default class Pokecard extends Component {
   render() {
-    let imgSrc = `${POKE_API}${this.props.id}.png`;
+    let imgSrc = `${POKE_API}${padToThree(this.props.id)}.png`;
     return (
       <div className="Pokecard">
         <h1 className="Pokecard-title">{this.props.name}</h1>
