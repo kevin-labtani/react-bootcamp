b363ca993b89b8ce6514d6cb4e9c59e25a5664ca
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-28 11:33:09 +0100

    setup poke project

---
+++ 5.pokedex/src/App.js
@@ -0,0 +1,9 @@
+import React from "react";
+import "./App.css";
+import "./Pokecard";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
---
+++ 5.pokedex/src/Pokecard.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+
+export default class Pokecard extends Component {
+  render() {
+    return (
+      <div>
+        <h1>POKECARD</h1>
+      </div>
+    )
+  }
+}
