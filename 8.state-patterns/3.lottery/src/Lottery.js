d3bff8261ef4ff4d1f100dfdedc0b711efc4d386
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-29 11:26:28 +0100

    setup lottery example

---
+++ 8.state-patterns/3.lottery/src/Lottery.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react'
+import LotteryBall from './LotteryBall'
+
+export default class Lottery extends Component {
+  render() {
+    return (
+      <div>
+        <LotteryBall />
+      </div>
+    );
+  }
+}
