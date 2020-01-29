0f5f74f28abc5154e7b6a04bfd47ed036b58d535
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-29 10:36:55 +0100

    setup Mutable Data Structures in State example

---
+++ 8.state-patterns/2.mutable-data-sctructures/src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import IconList from "./IconList"
+
+function App() {
+  return (
+    <div className="App">
+      <IconList />
+    </div>
+  );
+}
+
+export default App;
