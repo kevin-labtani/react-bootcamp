6af91cbab775dc20b7ddab2a74d478274b85a402
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-30 10:17:35 +0100

    react events-wisesquare example

---
+++ 10.react-events/1.events-demo/src/App.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./App.css";
+import WiseSquare from "./WiseSquare";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>React Events!</h1>
+      <WiseSquare />
+    </div>
+  );
+}
+
+export default App;
