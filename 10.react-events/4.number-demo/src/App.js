5fe5d0f471e55b0cf4e6c4c00d4ff05ff18cfcd1
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-30 11:43:02 +0100

    pass function as props

---
+++ 10.react-events/4.number-demo/src/App.js
@@ -0,0 +1,15 @@
+import React, { Component } from "react";
+import NumberList from "./NumberList";
+import "./App.css";
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <NumberList />
+      </div>
+    );
+  }
+}
+
+export default App;
