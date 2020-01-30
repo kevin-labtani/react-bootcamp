c926142353b6b9e7730a865eb7c1230636069589
Kevin Labtani <kevinlabtani@gmail.com>
2020-01-30 10:52:48 +0100

    binding this

---
+++ 10.react-events/2.binding-demo/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import WiseSquare from "./WiseSquare";
+import WiseSquareWithProps from "./WiseSquareWithProps";
+function App() {
+
+  return (
+    <div className="App">
+      <WiseSquare />
+      <WiseSquareWithProps />
+    </div>
+  );
+}
+
+export default App;
