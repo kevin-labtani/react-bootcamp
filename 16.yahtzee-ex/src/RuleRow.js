4462b1c449cd4f6dbcaad9dc57225f8fddce1e1a
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-01 18:43:48 +0100

    prevent score reuse- my solution

--- 16.yahtzee-ex/src/RuleRow.js
+++ 16.yahtzee-ex/src/RuleRow.js
@@ -3,12 +3,23 @@ import "./RuleRow.css";
 
 class RuleRow extends Component {
   render() {
-    return (
-      <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
-        <td className="RuleRow-name">{this.props.name}</td>
-        <td className="RuleRow-score">{this.props.score}</td>
-      </tr>
-    );
+    let ruleRow;
+    if (this.props.score === undefined) {
+      ruleRow = (
+        <tr className="RuleRow RuleRow-active" onClick={this.props.doScore}>
+          <td className="RuleRow-name">{this.props.name}</td>
+          <td className="RuleRow-score">{this.props.score}</td>
+        </tr>
+      );
+    } else {
+      ruleRow = (
+        <tr className="RuleRow RuleRow-disabled" onClick={null}>
+          <td className="RuleRow-name">{this.props.name}</td>
+          <td className="RuleRow-score">{this.props.score}</td>
+        </tr>
+      );
+    }
+    return ruleRow;
   }
 }
 
