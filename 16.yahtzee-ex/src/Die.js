4caa1be3df90893b011acc7279a7e31a550dab88
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-01 20:49:10 +0100

    add icons

--- 16.yahtzee-ex/src/Die.js
+++ 16.yahtzee-ex/src/Die.js
@@ -2,6 +2,10 @@ import React, { Component } from "react";
 import "./Die.css";
 
 class Die extends Component {
+  static defaultProps = {
+    numWords: ["one", "two", "three", "four", "five", "six"]
+  };
+
   constructor(props) {
     super(props);
 
@@ -13,16 +17,12 @@ class Die extends Component {
   handleClick() {
     this.props.handleClick(this.props.idx);
   }
-
   render() {
+    const { numWords, val, locked, disabled } = this.props;
+    let classes = `Die fas fa-5x fa-dice-${numWords[val - 1]} `;
+    if (locked) classes += "Die-locked";
     return (
-      <button
-        className={"Die"}
-        style={{ backgroundColor: this.props.locked ? "grey" : "black" }}
-        onClick={this.handleClick}
-      >
-        {this.props.val}
-      </button>
+      <i className={classes} onClick={this.handleClick} disabled={disabled} />
     );
   }
 }
