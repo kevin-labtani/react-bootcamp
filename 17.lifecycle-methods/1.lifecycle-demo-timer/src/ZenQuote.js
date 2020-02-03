aae0c93f8c7df14b43b944b5157f62ebe8c82170
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-03 14:53:57 +0100

    add loader css while we wait to fetch data from api

--- 17.lifecycle-methods/1.lifecycle-demo-timer/src/ZenQuote.js
+++ 17.lifecycle-methods/1.lifecycle-demo-timer/src/ZenQuote.js
@@ -1,27 +1,44 @@
 import React, { Component } from "react";
 import axios from "axios";
+import "./ZenQuote.css";
 
 export default class ZenQuote extends Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      quote: ""
+      quote: "",
+      isLoaded: false
     };
   }
 
   componentDidMount() {
     // load data from api
     axios.get("https://api.github.com/zen").then(res => {
-      this.setState({ quote: res.data });
+      // add delay to better show loading state
+      setTimeout(
+        function() {
+          this.setState({
+            quote: res.data,
+            isLoaded: true
+          });
+        }.bind(this),
+        2000
+      );
     });
   }
 
   render() {
     return (
       <div>
-        <h1>Always remember...</h1>
-        <p>{this.state.quote}</p>
+        {this.state.isLoaded ? (
+          <div>
+            <h1>Always remember...</h1>
+            <p>{this.state.quote}</p>
+          </div>
+        ) : (
+          <div className="loader"></div>
+        )}
       </div>
     );
   }
