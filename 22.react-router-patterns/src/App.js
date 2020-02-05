064e38ba2b812a8d38ad9aa8af5e1a636b307b43
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-05 11:11:46 +0100

    multiple route prams

--- 22.react-router-patterns/src/App.js
+++ 22.react-router-patterns/src/App.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import "./App.css";
 import Food from "./Food";
+import Meal from "./Meal";
 
 export default class App extends Component {
   render() {
@@ -11,7 +12,14 @@ export default class App extends Component {
           <Route
             exact
             path="/food/:name"
+            // component={Food}
             render={routeProps => <Food {...routeProps} />}
+          />
+
+          <Route
+            exact
+            path="/food/:foodName/drink/:drinkName"
+            component={Meal}
           />
         </div>
       </Router>
