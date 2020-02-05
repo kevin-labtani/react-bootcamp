11b6eddc4f18ddf3dee1b046273c5e1a678e83fe
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-05 12:27:34 +0100

    back button

--- 22.react-router-patterns/src/Navbar.js
+++ 22.react-router-patterns/src/Navbar.js
@@ -1,11 +1,13 @@
 import React, { Component } from "react";
 import { withRouter } from "react-router-dom";
+import "./Navbar.css";
 
 class Navbar extends Component {
   constructor(props) {
     super(props);
 
     this.handleLogin = this.handleLogin.bind(this);
+    this.handleBack = this.handleBack.bind(this);
   }
 
   handleLogin() {
@@ -13,10 +15,15 @@ class Navbar extends Component {
     this.props.history.push("/food/salmon");
   }
 
+  handleBack() {
+    this.props.history.goBack();
+  }
+
   render() {
     return (
-      <div>
+      <div className="Navbar">
         <button onClick={this.handleLogin}>Log In</button>
+        <button onClick={this.handleBack}>Go Back</button>
       </div>
     );
   }
