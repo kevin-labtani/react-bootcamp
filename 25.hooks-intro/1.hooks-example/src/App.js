261c30c2bae8cc6d2984f9c96a6abedfae33f5cc
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-10 20:54:20 +0100

    build custom hook useToggle

--- 25.hooks-intro/1.hooks-example/src/App.js
+++ 25.hooks-intro/1.hooks-example/src/App.js
@@ -2,13 +2,15 @@ import React from "react";
 import "./App.css";
 import CounterClass from "./CounterClass";
 import CounterHooks from "./CounterHooks";
+import Toggler from "./Toggler";
 
 class App extends React.Component {
   render() {
     return (
       <div className="App">
         <CounterClass />
-        <CounterHooks/>
+        <CounterHooks />
+        <Toggler/>
       </div>
     );
   }
---
+++ 25.hooks-intro/1.hooks-example/src/Toggler.js
@@ -0,0 +1,16 @@
+import React from "react";
+import useToggle from "./hooks/useToggle";
+
+function Toggler() {
+  const [isHappy, toggleIsHappy] = useToggle(true);
+  const [isHeartBroken, toggleIsHeartBroken] = useToggle(false);
+
+  return (
+    <div>
+      <h1 onClick={toggleIsHappy}>{isHappy ? "😄" : "😢"}</h1>
+      <h1 onClick={toggleIsHeartBroken}>{isHeartBroken ? "💔" : "❤️"}</h1>
+    </div>
+  );
+}
+
+export default Toggler;
