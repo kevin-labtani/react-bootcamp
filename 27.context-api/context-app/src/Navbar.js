2f830e6345775be8bf172d3d9fd0e1fc7c675ca5
Kevin Labtani <kevinlabtani@gmail.com>
2020-02-26 12:01:49 +0100

    consuming context

--- 27.context-api/context-app/src/Navbar.js
+++ 27.context-api/context-app/src/Navbar.js
@@ -8,13 +8,18 @@ import Switch from "@material-ui/core/Switch";
 import SearchIcon from "@material-ui/icons/Search";
 import { withStyles } from "@material-ui/core/styles";
 import styles from "./styles/NavBarStyles";
+import { ThemeContext } from "./contexts/ThemeContext";
 
 class Navbar extends Component {
+  static contextType = ThemeContext;
+
   render() {
+    // console.log(this.context); // the piece of data isDarkMode is accessible here now
+    const { isDarkMode } = this.context;
     const { classes } = this.props; //passed from withStyles
     return (
       <div className={classes.root}>
-        <AppBar position="static" color="primary">
+        <AppBar position="static" color={isDarkMode ? "default" : "primary"}>
           <Toolbar>
             <IconButton className={classes.menuButton} color="inherit">
               <span>🇫🇷</span>
--- 27.context-api/context-app/src/PageContent.js
+++ 27.context-api/context-app/src/PageContent.js
@@ -1,10 +1,13 @@
 import React, { Component } from "react";
+import { ThemeContext } from "./contexts/ThemeContext";
 
 // this component exists so we can change the backgroud color
 class PageContent extends Component {
+  static contextType = ThemeContext;
   render() {
+    const { isDarkMode } = this.context;
     const styles = {
-      backgroundColor: "white",
+      backgroundColor: isDarkMode ? "#333" : "white",
       height: "100vh",
       width: "100vw"
     };
