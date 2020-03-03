af136db7eec14977547646ad7b307cdc7f1a2871
Kevin Labtani <kevinlabtani@gmail.com>
2020-03-03 14:59:39 +0100

    query string using withRouter hoc

--- 30.nextjs/first-next/pages/post.js
+++ 30.nextjs/first-next/pages/post.js
@@ -1,8 +1,15 @@
-const Post = ({ id }) => {
-  return <h1>You are looking at post # {id}</h1>;
-};
-Post.getInitialProps = async ({ query }) => {
-  return query;
-};
+import { withRouter } from "next/router";
+
+const Post = withRouter(props => {
+  return <h1>You are looking at post # {props.router.query.id} </h1>;
+});
+
+// alternative to withrouter to get the query string
+// const Post = ({ id }) => {
+//   return <h1>You are looking at post # {id}</h1>;
+// };
+// Post.getInitialProps = async ({ query }) => {
+//   return query;
+// };
 
 export default Post;
